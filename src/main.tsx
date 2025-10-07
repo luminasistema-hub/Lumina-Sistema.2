15c9c2ba09fa0b06b62fca8eb1704c8546d0beeb
[dyad] <luminasistema@gmail.com>
2025-10-07 01:10:29 -0300

    [dyad] Instalando e ativando Vercel Speed Insights - wrote 4 file(s), added @vercel/speed-insights package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from './App.tsx'
 import './index.css'
 import ReconnectManager from './components/system/ReconnectManager'
@@ -43,6 +44,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
+        <SpeedInsights />
         <Toaster />
         <ReconnectManager />
       </BrowserRouter>
