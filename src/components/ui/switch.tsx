8d55e9399285d0867ce1524f670b1256b6907043
[dyad] <luminasistema@gmail.com>
2025-10-22 01:26:45 -0300

    [dyad] Corrigido o comportamento visual do Switch - wrote 1 file(s)

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -12,20 +12,20 @@ const Switch = React.forwardRef<
     ref={ref}
     className={cn(
       "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors",
-      "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       "data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className
     )}
     {...props}
   >
-    <span
+    <SwitchPrimitives.Thumb
       className={cn(
         "pointer-events-none block h-5 w-5 rounded-full bg-white shadow-lg ring-0 transition-transform",
-        "translate-x-0 data-[state=checked]:translate-x-5"
+        "data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
 ));
-Switch.displayName = "Switch";
+Switch.displayName = SwitchPrimitives.Root.displayName;
 
 export { Switch };
