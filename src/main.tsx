520b39337f21b6fd2f217e6caac22c919659b4fd
[dyad] <luminasistema@gmail.com>
2025-10-23 02:21:32 -0300

    [dyad] Resolvendo conflito de bibliotecas de toast - wrote 2 file(s), deleted 4 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Toaster } from 'sonner'
+import { Toaster } from '@/components/ui/sonner'
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './components/system/ThemeProvider'
 
 // Limpa cache do navegador ao iniciar
 if ('caches' in window) {
@@ -43,9 +44,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
-        <SpeedInsights />
-        <Toaster />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <App />
+          <SpeedInsights />
+          <Toaster />
+        </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
